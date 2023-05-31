7d6db7b180736ddfd30b8b333a18b7645bdbfbc9
André <andregelabert21@gmail.com>
2023-05-31 19:06:36 -0300

    TP4Ejercicio2 V2

--- TP4Ejercicio2.java
+++ TP4Ejercicio2.java
@@ -42,42 +42,61 @@ class Alumno {
 public class TP4Ejercicio2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Ingrese la cantidad de alumnos a cargar: ");
-        int cantidadAlumnos = scanner.nextInt();
-
-        ArrayList<Alumno> alumnos = new ArrayList<>();
-
-        for (int i = 0; i < cantidadAlumnos; i++) {
-            System.out.println("Ingrese los datos del alumno " + (i + 1) + ":");
-            System.out.print("Legajo: ");
-            int legajo = scanner.nextInt();
-            System.out.print("Nombre: ");
-            String nombre = scanner.next();
-            System.out.print("Apellido: ");
-            String apellido = scanner.next();
-            System.out.print("Nota: ");
-            int nota = scanner.nextInt();
-
-            Alumno alumno = new Alumno(legajo, nombre, apellido, nota);
-            alumnos.add(alumno);
-        }
-
-        System.out.println("\nDatos de los alumnos:");
-
-        for (Alumno alumno : alumnos) {
-            System.out.println(alumno);
-        }
-
-        // Ordenar el ArrayList en orden decreciente según la nota de cada alumno
-        Collections.sort(alumnos, Comparator.comparingInt(Alumno::getNota).reversed());
-
-        System.out.println("\nDatos de los alumnos ordenados por nota (decreciente):");
-
-        for (Alumno alumno : alumnos) {
-            System.out.println(alumno);
-        }
-        scanner.close();
+        boolean numIngresado;
+        String textnumcant;
+        int cantidadAlumnos;
+        ArrayList<Alumno> alumnos;
+        do {
+            System.out.print("Ingrese la cantidad de alumnos a cargar: ");
+            textnumcant = scanner.nextLine();
+            numIngresado = false;
+            if (textnumcant.isEmpty()) {
+                System.out.println("Error, no ha ingresado nada. Ingrese algo por favor");
+            } else {
+                do {
+                    try {
+                        cantidadAlumnos = Integer.parseInt(textnumcant);
+                        alumnos = new ArrayList<>();
+                        for (int i = 0; i < cantidadAlumnos; i++) {
+                            System.out.println("Ingrese los datos del alumno " + (i + 1) + ":");
+                            System.out.print("Legajo: ");
+                            int legajo = scanner.nextInt();
+                            System.out.print("Nombre: ");
+                            String nombre = scanner.next();
+                            System.out.print("Apellido: ");
+                            String apellido = scanner.next();
+                            System.out.print("Nota: ");
+                            int nota = scanner.nextInt();
+                
+                            Alumno alumno = new Alumno(legajo, nombre, apellido, nota);
+                            alumnos.add(alumno);
+                        }
+                
+                        System.out.println("\nDatos de los alumnos:");
+                
+                        for (Alumno alumno : alumnos) {
+                            System.out.println(alumno);
+                        }
+                
+                        // Ordenar el ArrayList en orden decreciente según la nota de cada alumno
+                        Collections.sort(alumnos, Comparator.comparingInt(Alumno::getNota).reversed());
+                
+                        System.out.println("\nDatos de los alumnos ordenados por nota (decreciente):");
+                
+                        for (Alumno alumno : alumnos) {
+                            System.out.println(alumno);
+                        }
+                        numIngresado = true;
+                        scanner.close();
+                    } catch (NumberFormatException e) {
+                        System.out.println("El valor ingresado no es un número válido.");
+                        numIngresado = false;
+                        System.out.print("Ingrese un número entero: ");
+                        textnumcant = scanner.nextLine();
+                    }
+                } while (!numIngresado);
+            }
+        } while (textnumcant.isEmpty());
     }
 }
 
